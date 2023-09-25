12c685251dcb098276b6dc8d2405569bf7cddde8
Joshua <jpe7619@gmail.com>
2023-09-24 18:42:05 -0700

    initialize electron

--- scoreBoard.py
+++ scoreBoard.py
@@ -1,61 +1,85 @@
-import tkinter as tk
-from tkcalendar import DateEntry
+import sys
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QWidget,
+    QLabel,
+    QTextEdit,
+    QPushButton,
+    QVBoxLayout,
+    QDateEdit,
+)
+from PyQt5.QtCore import Qt, QDate
 import requests
-from tkinter.scrolledtext import ScrolledText
 
 
-def fetch_nfl_games_data():
-    selected_date = date_picker.get_date()
-    api_url = ""
-    headers = {
-        "X-Rapidapi-Key": "",
-        "X-Rapidapi-Host": "",
-    }
+class NFLGamesApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("NFL Games Information")
+        self.setGeometry(100, 100, 400, 400)
+
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+
+        layout = QVBoxLayout()
+        self.central_widget.setLayout(layout)
+
+        self.date_picker = QDateEdit()
+        self.date_picker.setCalendarPopup(True)  # Enable calendar popup
+        self.date_picker.setDate(QDate.currentDate())  # Set default date to today
+        layout.addWidget(self.date_picker)
+
+        self.games_info_text = QTextEdit()
+        self.games_info_text.setReadOnly(True)
+        layout.addWidget(self.games_info_text)
+
+        fetch_button = QPushButton("Fetch NFL Games Data")
+        fetch_button.clicked.connect(self.fetch_nfl_games_data)
+        layout.addWidget(fetch_button)
+
+    def fetch_nfl_games_data(self):
+        selected_date = self.date_picker.date().toString("yyyy-MM-dd")
+        api_url = "https://api-american-football.p.rapidapi.com/games"
+        headers = {
+            "X-Rapidapi-Key": "API KEY",
+            "X-Rapidapi-Host": "api-american-football.p.rapidapi.com",
+        }
+
+        params = {"date": selected_date, "league": 1}
+
+        try:
+            response = requests.get(api_url, headers=headers, params=params)
+
+            if response.status_code == 200:
+                data = response.json()
+                games_today = len(data["response"])  # Count of games for today
+                game_info = f"Date: {selected_date}\n"
+                game_info += f"Games today: {games_today}\n\n"
+
+                for game in data["response"]:
+                    time = game["game"]["date"]["time"]
+                    home_team = game["teams"]["home"]["name"]
+                    away_team = game["teams"]["away"]["name"]
+
+                    game_info += f"Time (UTC): {time}\n"
+                    game_info += f"Home Team: {home_team}\n"
+                    game_info += f"Away Team: {away_team}\n\n"
+
+                self.games_info_text.setPlainText(game_info)
+
+            else:
+                print(
+                    f"Failed to fetch NFL games data. Status Code: {response.status_code}"
+                )
+                print(response.text)
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
 
 
-    params = {"date": selected_date, "league": 1}
-
-    try:
-        response = requests.get(api_url, headers=headers, params=params)
-
-        if response.status_code == 200:
-            data = response.json()
-            games_today = len(data['response'])  # Count of games for today
-            games_today_label.config(text=f"Games today: {games_today}")
-
-            game_info_text.delete("1.0", tk.END)
-
-            for game in data['response']:
-                time = game['game']['date']['time']
-                home_team = game["teams"]["home"]["name"]
-                away_team = game["teams"]["away"]["name"]
-
-                game_info_text.insert(tk.END, f"Time (UTC): {time}\n")
-                game_info_text.insert(tk.END, f"Home Team: {home_team}\n")
-                game_info_text.insert(tk.END, f"Away Team: {away_team}\n")
-                game_info_text.insert(tk.END, "\n")
-
-        else:
-            print(f"Failed to fetch NFL games data. Status Code: {response.status_code}")
-            print(response.text)
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-
-
-root = tk.Tk()
-root.title("NFL Games Information")
-root.geometry("400x400")
-
-date_picker = DateEntry(root, width=12)
-date_picker.pack()
-
-games_today_label = tk.Label(root, text="Games today: 0")
-games_today_label.pack()
-
-game_info_text = ScrolledText(root, height=10, width=40)
-game_info_text.pack(fill=tk.BOTH, expand=True)
-
-fetch_button = tk.Button(root, text="Fetch NFL Games Data", command=fetch_nfl_games_data)
-fetch_button.pack()
-
-root.mainloop()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = NFLGamesApp()
+    window.show()
+    sys.exit(app.exec_())
